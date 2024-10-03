0bb1669036eb2c6016ef871260620c66fd6897e0
Sai Pulaparthi <sai@Sais-MacBook-Air-2.local>
2024-10-03 12:52:51 -0400

    Intials2

--- main.cpp
+++ main.cpp
@@ -98,24 +98,3 @@ int main()
 
 
 
-
-   /* Overall flow of the program:
-   Get some input for the title
-   Get some input for column header #1
-   Get some input for column header #2
-
-   Until the user types "-1":
-   Get input for a string
-   Validate input
-   If valid...
-      Split
-      Validate second half of input
-      If also valid...
-         Store separate pieces
-
-   Print out data in table format
-
-   Print out data in chart format
-   */
-// WRITE YOUR CODE HERE
-
